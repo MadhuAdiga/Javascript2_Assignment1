2ec6904b91d4d30a983c9b11675a3fc9559f85ee
MadhuAdiga <madhu.vah@gmail.com>
2017-01-04 09:21:04 +0530

    Assignment1

---
+++ js/app.js
@@ -0,0 +1,3 @@
+var radius=prompt("Enter a radius value in cm"); //Takes in a radius value
+var area=Math.PI*(radius**2); //Calculates the area
+console.log("Area for the circle of entered radius is "+ area + "cm2");//Outputs the area value in console
